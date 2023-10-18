c97f2ace26f8c3e8be866911fae261f481e47c18
ClaytonDewey <clay@dryadmedia.com>
2023-10-18 14:41:57 -0500

    chore: Set up Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,5 +8,10 @@ import { HiOutlineChatBubbleBottomCenterText } from 'react-icons/hi2'
 import { FiSettings, FiShoppingCart } from 'react-icons/fi'
 
 export const Sidebar = () => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef(null);
+  useClickAway(ref, () => setOpen(false))
+  const toggleSidebar = () => setOpen(prev => !prev);
+
   return <div>Sidebar</div>
 }
