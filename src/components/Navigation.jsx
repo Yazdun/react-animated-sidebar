c0759e4c62b6abdd11d3efb5a2a6bed3aa142699
ClaytonDewey <clay@dryadmedia.com>
2023-10-18 14:39:59 -0500

    feat: Create Navigation component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,5 +2,15 @@ import { Sidebar } from './Sidebar'
 import { FiGithub } from 'react-icons/fi'
 
 export const Navigation = () => {
-  return <nav>Navbar</nav>
+  return (
+    <nav className='flex items-center justify-between px-5 py-2 border-b-2 border-zinc-800'>
+      <div className="flex items-center gap-3">
+        <Sidebar />
+        <p>Animated Sidebar</p>
+      </div>
+      <a href="/" className="flex items-center gap-2 px-4 py-2 text-orange-400 bg-orange-700/20 rounded-xl">
+        <FiGithub className='text-lg' />
+      </a>
+    </nav>
+  )
 }
